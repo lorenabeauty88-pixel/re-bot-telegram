82dde9c29f8d185c45492b36dce2a045b6daa004
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-13 20:54:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const TelegramBot = require('node-telegram-bot-api');
 // Token vindo do Render (Environment Variables)
 const token = process.env.TOKEN;
 
-
 const bot = new TelegramBot(token, { polling: true });
 
 // Responde qualquer mensagem
