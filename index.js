00e3b392ad337ce43e18a1827c2f16430df7e4dd
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-13 19:00:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Token vindo do Render (Environment Variables)
-const token = process.env.token;
+const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
