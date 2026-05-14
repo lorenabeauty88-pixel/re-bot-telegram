295ad7cadab9ea78aac29a8ca7f803495c6ecc30
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-14 15:30:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Bot online!");
+  res.send("Bot funcionando!");
 });
 
 const PORT = process.env.PORT || 3000;
