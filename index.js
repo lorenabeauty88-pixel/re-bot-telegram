33b99b6240efb26ad7a229c920fbc94c57c27b2b
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-13 21:09:33 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Token vindo do Render (Environment Variables)
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-// Responde qualquer mensagem
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Bot funcionando 👍');
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(
+        msg.chat.id,
+`🌸 Bem-vinda ao Rê Recomenda 🌸
+
+Envie:
+
+/promo LINK
+
+Exemplo:
+/promo https://amzn.to/teste`
+    );
+});
+
+bot.onText(/\/promo (.+)/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    const link = match[1];
+
+    const mensagem = `
+🌸 *RÊ RECOMENDA* 🌸
+
+✨ Oferta especial encontrada!
+
+🛍️ Produto incrível com preço imperdível 💖
+
+🔗 ${link}
+
+🚀 Aproveite antes que acabe!
+`;
+
+    bot.sendMessage(chatId, mensagem, {
+        parse_mode: 'Markdown'
+    });
 });
