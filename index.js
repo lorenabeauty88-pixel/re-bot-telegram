261442deea3db914c78367761d315be3050c7e45
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-15 09:26:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,65 +2,25 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.BOT_TOKEN;
 
+console.log("TOKEN:", token);
+
 const bot = new TelegramBot(token, {
   polling: true
 });
 
-console.log("✅ Bot online...");
+console.log("✅ Bot iniciado");
 
-bot.onText(/\/start/, (msg) => {
+bot.on("message", (msg) => {
+
+  console.log("Mensagem recebida");
 
   bot.sendMessage(
     msg.chat.id,
-
-`🌸 BOT DE OFERTAS 🌸
-
-Envie:
-
-/promo link
-
-Exemplo:
-/promo https://google.com`
+    "🔥 BOT FUNCIONANDO 🔥"
   );
 
 });
 
-bot.onText(/\/promo (.+)/, (msg, match) => {
-
-  const chatId = msg.chat.id;
-  const link = match[1];
-
-  const foto =
-    "https://i.imgur.com/2s9XK4p.jpeg";
-
-  bot.sendPhoto(chatId, foto, {
-
-    caption:
-`🔥 SUPER OFERTA 🔥
-
-🛍️ Produto em promoção
-🚚 Frete disponível
-⏰ Oferta limitada
-
-👇 Clique no botão abaixo 👇`,
-
-    reply_markup: {
-
-      inline_keyboard: [
-        [
-          {
-            text: "🛒 VER OFERTA",
-            url: link
-          }
-        ]
-      ]
-
-    }
-
-  });
-
-});
-
 bot.on("polling_error", (error) => {
-  console.log(error.message);
+  console.log(error);
 });
