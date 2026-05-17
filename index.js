d25ea387f640c1f32523da7ce0d6d13986b4cfac
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-17 01:07:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const token = process.env.BOT_TOKEN;
 
-console.log("BOT ONLINE TESTE");
+console.log("BOT START");
+
+if (!token) {
+  console.log("SEM TOKEN");
+  process.exit(1);
+}
+
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg) => {
   console.log("RECEBI:", msg.text);
 
-  bot.sendMessage(
-    msg.chat.id,
-    "🔥 FUNCIONANDO 100% - RECEBI SUA MENSAGEM"
-  );
+  bot.sendMessage(msg.chat.id, "🔥 FUNCIONANDO - RECEBI SUA MENSAGEM");
 });
