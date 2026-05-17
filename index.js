9c7f0e88baf7dcf8a3933047f565f5d62cc135c8
lorenabeauty88-pixel <lorenabeauty88@gmail.com>
2026-05-17 01:16:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const TelegramBot = require("node-telegram-bot-api");
-
-console.log("INICIANDO BOT...");
+console.log("INICIANDO...");
 
 console.log("TOKEN:", process.env.BOT_TOKEN);
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
-bot.on("polling_error", (err) => {
-  console.log("POLLING ERROR:", err);
-});
-
-bot.on("message", (msg) => {
-  console.log("RECEBI:", msg.text);
-
-  bot.sendMessage(msg.chat.id, "🔥 FUNCIONANDO!");
-});
+process.exit(0);
